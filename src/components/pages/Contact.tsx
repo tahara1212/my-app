44e5ca08ac4386c20897712318ececdba78810fe
田原隼併 <t.pe1shun@gmail.com>
2021-09-18 23:14:59 +0900

    create form

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useContext, VFC } from "react";
 import styled from "styled-components";
 import { useInView } from "react-intersection-observer";
-import { TextField } from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
 import { useForm, Controller } from "react-hook-form";
+import Tooltip from "@material-ui/core/Tooltip";
 
 import { TitleTextContext } from "../../App";
 import { Container } from "../templates/Container";
@@ -34,25 +35,51 @@ export const Contact: VFC = () => {
       <SContact ref={ref}>
         <SContactSnsBox></SContactSnsBox>
         <SContactFormBox>
-          <form onSubmit={handleSubmit(on_submit)}>
-            <Controller
-              control={control}
-              {...register("name", { required: true })}
-              render={({ field }) => (
-                <STextField id="standard-basic" label="Name" {...field} />
-              )}
-            />
-            {errors.name && <p>Your input is required</p>}
-            <Controller
-              control={control}
-              {...register("email", { required: true })}
-              render={({ field }) => (
-                <STextField id="standard-basic" label="Email" {...field} />
-              )}
-            />
-            {errors.email && <p>Your input is required</p>}
-            <button type="submit">Submit</button>
-          </form>
+          <SContactInputArea>
+            <form onSubmit={handleSubmit(on_submit)}>
+              <Controller
+                control={control}
+                {...register("name", { required: true })}
+                render={({ field }) => (
+                  <STextField id="standard-basic" label="Name" {...field} />
+                )}
+              />
+              {errors.name && <p>Your input is required</p>}
+              <Controller
+                control={control}
+                {...register("email", { required: true })}
+                render={({ field }) => (
+                  <STextField id="standard-basic" label="Email" {...field} />
+                )}
+              />
+              {errors.email && <p>Your input is required</p>}
+              <Controller
+                control={control}
+                name="message"
+                render={({ field }) => (
+                  <Tooltip
+                    title="You can fill in freely"
+                    placement="top-start"
+                    arrow
+                  >
+                    <TextField
+                      {...field}
+                      label="Msessage"
+                      fullWidth
+                      margin="normal"
+                      rows={4}
+                      multiline
+                      variant="outlined"
+                      placeholder="Message"
+                    />
+                  </Tooltip>
+                )}
+              />
+              <Button variant="contained" type="submit">
+                Submit
+              </Button>
+            </form>
+          </SContactInputArea>
         </SContactFormBox>
       </SContact>
     </Container>
@@ -77,8 +104,19 @@ const SContactSnsBox = styled.div`
 
 const SContactFormBox = styled.div`
   width: 50%;
+  position: relative;
+`;
+
+const SContactInputArea = styled.div`
+  width: 60%;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-25%, -50%);
 `;
 
 const STextField = styled(TextField)`
   color: black;
+  width: 100%;
 `;
