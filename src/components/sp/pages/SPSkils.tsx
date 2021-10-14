65255f994395e23052a2b1f10a8afc6eb7509071
田原隼併 <t.pe1shun@gmail.com>
2021-10-14 18:25:10 +0900

    SP Skils

--- src/components/sp/pages/SPSkils.tsx
+++ src/components/sp/pages/SPSkils.tsx
@@ -1,19 +1,29 @@
 import { memo, useContext, useEffect, VFC } from "react";
 import styled from "styled-components";
 import { useInView } from "react-intersection-observer";
-import {
-  Chart,
-  BarSeries,
-  ArgumentAxis,
-  ValueAxis,
-} from "@devexpress/dx-react-chart-bootstrap4";
 import "@devexpress/dx-react-chart-bootstrap4/dist/dx-react-chart-bootstrap4.css";
-import { Animation } from "@devexpress/dx-react-chart";
+import {
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  Radar,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-import variable from "../../../css/variables.json";
+// import variable from "../../../css/variables.json";
 import { TitleTextContext } from "../../../App";
 import { Container } from "../../templates/Container";
-import { SkilsData } from "../../../data/SkilsData";
+// import { SkilsData } from "../../../data/SkilsData";
+
+//表示させたいデータ群
+const dataRadar = [
+  { rank: "Markup", value: 75 },
+  { rank: "FrontEnd", value: 70 },
+  { rank: "BackEnd", value: 65 },
+  { rank: "ServerSide", value: 50 },
+  { rank: "Design", value: 35 },
+];
 
 export const SPSkils: VFC = memo(() => {
   const { setTitle } = useContext(TitleTextContext);
@@ -33,17 +43,28 @@ export const SPSkils: VFC = memo(() => {
     <Container id="Skils">
       <SSkils ref={ref}>
         {inView && (
-          <Chart data={SkilsData}>
-            <ArgumentAxis />
-            <ValueAxis showLabels={false} />
-            <BarSeries
-              valueField="area"
-              argumentField="skil"
-              color={variable.subColor}
-              barWidth={0.5}
-            />
-            <Animation />
-          </Chart>
+          <ResponsiveContainer>
+            <RadarChart // レーダーチャートのサイズや位置、データを指定
+              height={400} //レーダーチャートの全体の高さを指定
+              width={500} //レーダーチャートの全体の幅を指定
+              cx="50%" //要素の左を基準に全体の50%移動
+              cy="50%" //要素の上を基準に全体の50%移動
+              data={dataRadar} //ここにArray型のデータを指定
+            >
+              <PolarGrid />
+              <PolarAngleAxis
+                dataKey="rank" //Array型のデータの、数値を表示したい値のキーを指定
+              />
+              <Radar //レーダーの色や各パラメーターのタイトルを指定
+                name="ability" //hoverした時に表示される名前を指定
+                dataKey="value" //Array型のデータのパラメータータイトルを指定
+                stroke="#8884d8" //レーダーの線の色を指定
+                fill="#8884d8" //レーダーの中身の色を指定
+                fillOpacity={0.6} //レーダーの中身の色の薄さを指定
+              />
+              <Tooltip />
+            </RadarChart>
+          </ResponsiveContainer>
         )}
       </SSkils>
     </Container>
@@ -57,4 +78,6 @@ const SSkils = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -35%);
+  font-size: 1.8vw;
+  overflow-wrap: break-word;
 `;
